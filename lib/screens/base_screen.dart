a872231b2580f392c32675980d2c59568041fb07
Marcelo <mar.rose1999@gmail.com>
2020-01-22 22:17:02 -0300

    Project clone and PageView initied

---
+++ lib/screens/base_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatefulWidget {
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: PageView(
+            physics: const NeverScrollableScrollPhysics(),
+            children: <Widget>[
+                Container(color: Colors.red),
+                Container(color: Colors.blue),
+                Container(color: Colors.red),
+                Container(color: Colors.blue),
+                Container(color: Colors.red),
+            ],
+        ),
+    );
+  }
+}
